c08a5b5a465dad17c74f498d52e05f40baeb2969
jalaliamin <jalali.amin@gmail.com>
2018-12-31 22:39:51 +0100

    Add files via upload

---
+++ ubuntoFlask/data/settScript.sh
@@ -0,0 +1,8 @@
+yes | apt-get update 
+yes | apt-get install sudo 
+yes | apt-get install nano 
+yes | apt-get install python3 
+yes | sudo apt-get install python-pip 
+yes | pip install flask numpy pandas 
+
+
---
+++ ubuntoFlask/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash 
+
+docker run -d --rm -p 5000:5000 ubuntuflask
