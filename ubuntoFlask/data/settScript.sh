6b2bd8fdcf77c81ac0311369cf45f5c9be9b20a6
jalaliamin <jalali.amin@gmail.com>
2019-01-02 11:20:20 +0100

    Update settScript.sh

--- ubuntoFlask/data/settScript.sh
+++ ubuntoFlask/data/settScript.sh
@@ -1,4 +1,4 @@
-yes | apt-get install nano
-yes | apt-get install python3
-yes | sudo apt-get install python-pip
-yes | pip install flask numpy pandas
+yes | apt-get install nano;
+yes | apt-get install python3;
+yes | sudo apt-get install python-pip;
+yes | pip install flask numpy pandas;
